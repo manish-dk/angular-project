fa0378fc80313c4033100945f8788187422ccd8f
manish <manish34dk@gmail.com>
2020-08-06 04:08:05 +0100

    fix tetris

--- src/app/tetris/tetris.component.ts
+++ src/app/tetris/tetris.component.ts
@@ -18,12 +18,15 @@ export class TetrisComponent implements OnInit, AfterViewInit {
   p: number = 20;
   x: number = 0.5 + 12 * this.p;
   y: number = 0.5;
-  floor: number = 0.5 + 30 * this.p; //600.5, 380.5, 0.5 = coordinates of board
+  height:number = 21;
+  width:number = 16;
+  floor: number = 0.5 + (this.height - 1) * this.p; //600.5, 380.5, 0.5 = coordinates of board
   leftBorder:number = 0.5;
-  rightBorder:number = 380.5;
+  rightBorder:number = 0.5 + this.width * (this.p -1);
   tickrate:number = 500;
   score:number = 0;
   inDelay:boolean = false;
+  gameEnded:boolean = true;
 
   //tetrominos
   line = { state: 1, colour: "red", blocks: [[this.x, this.y], [this.x, this.y - this.p], [this.x, this.y - 2 * this.p], [this.x, this.y - 3 * this.p]] };
@@ -46,9 +49,10 @@ export class TetrisComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.ctx = this.canvas.nativeElement.getContext('2d');
-    this.canvas.nativeElement.height = 0.5 + 31 * this.p;
-    this.canvas.nativeElement.width = 0.5 + 20 * this.p;
+    this.canvas.nativeElement.height = 0.5 + this.height * this.p;
+    this.canvas.nativeElement.width = 0.5 + this.width * this.p;
     this.drawboard();
+    
   }
 
   ngAfterViewInit(): void {
@@ -57,7 +61,14 @@ export class TetrisComponent implements OnInit, AfterViewInit {
   }
 
   begin():void {
-    this.changeTick();
+    this.reset();
+    this.gameEnded = false;
+    this.gameDown = setInterval(this.downtick, this.tickrate);
+    this.gameRefresh = setInterval(this.refresh, this.tickrate);
+
+  }
+
+  continue():void {
     this.gameDown = setInterval(this.downtick, this.tickrate);
     this.gameRefresh = setInterval(this.refresh, this.tickrate);
 
@@ -65,12 +76,33 @@ export class TetrisComponent implements OnInit, AfterViewInit {
   reset():void{
     clearInterval(this.gameDown);
     clearInterval(this.gameRefresh);
+    clearInterval(this.speedUp);
     this.current = JSON.parse(JSON.stringify(this.tetrominos[Math.floor(Math.random() * this.tetrominos.length)]));
-    this.past = [];
     this.tickrate = 500;
+    this.gameEnded =true;
+    this.past = [];
     this.score = 0;
     this.ngOnInit();
   }
+
+  endGame() {
+    clearInterval(this.gameDown);
+    clearInterval(this.gameRefresh);
+    clearInterval(this.speedUp);
+    this.current = JSON.parse(JSON.stringify(this.tetrominos[Math.floor(Math.random() * this.tetrominos.length)]));
+    this.tickrate = 500;
+    this.gameEnded =true;
+    window.alert(`Game Over Score: ${this.score}`);
+  }
+
+  downtick = () => {
+    this.changeTick();
+    for (let i = 0; i < this.current.blocks.length; i++) {
+      this.current.blocks[i][1] = this.current.blocks[i][1] < this.floor - this.p ? this.current.blocks[i][1] + this.p : this.floor;
+    }
+    this.refreshFromKey();
+  }
+
   changeTick(){
       if(this.tickrate > 50) {
           this.tickrate -= 1;
@@ -81,15 +113,12 @@ export class TetrisComponent implements OnInit, AfterViewInit {
     this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
     this.drawboard();
     this.drawCurrent();
-    
     this.checkLine();
     this.drawPast();
     clearInterval(this.gameDown);
     clearInterval(this.gameRefresh);
-    this.changeTick();
     this.gameDown = setInterval(this.downtick, this.tickrate);
     this.gameRefresh = setInterval(this.refresh, this.tickrate);
-    this.checkCollision();
     
   }
 
@@ -127,26 +156,27 @@ export class TetrisComponent implements OnInit, AfterViewInit {
   @HostListener('window:keydown', ['$event'])
   handleEvent = (event: KeyboardEvent) => {
     if (event.keyCode == 38) { //119
+        if (this.gameEnded) return;
         this.rotate();
         this.refreshFromKey();
 
     }
     if (event.keyCode == 37) {
-        if (!this.checkMove(-1)) return;
+        if (!this.checkMove(-1) || this.gameEnded) return;
         for (let i = 0; i < this.current.blocks.length; i++) {
           this.current.blocks[i][0] -= this.p;
         }
         this.refreshFromKey();
     }
     if (event.keyCode == 39) {
-        if (!this.checkMove(1)) return;
+        if (!this.checkMove(1) || this.gameEnded) return;
         for (let i = 0; i < this.current.blocks.length; i++) {
           this.current.blocks[i][0] += this.p;
         }
         this.refreshFromKey();
     }
     if (event.keyCode == 40) {
-        if(this.inDelay){
+        if(this.inDelay || this.gameEnded){
             return;
         }
         for (let i = 0; i < this.current.blocks.length; i++) {
@@ -157,12 +187,7 @@ export class TetrisComponent implements OnInit, AfterViewInit {
   }
 
 
-  downtick = () => {
-    for (let i = 0; i < this.current.blocks.length; i++) {
-      this.current.blocks[i][1] = this.current.blocks[i][1] < this.floor - this.p ? this.current.blocks[i][1] + this.p : this.floor;
-    }
-    this.checkCollision();
-  }
+  
 
   drawCurrent = () => {
     this.ctx.fillStyle = this.current.colour;
@@ -173,24 +198,20 @@ export class TetrisComponent implements OnInit, AfterViewInit {
 
   storetetromino = async (tetromino) => {
         this.past.push(tetromino);
-        // tetroState = 1;
         this.current = JSON.parse(JSON.stringify(this.tetrominos[Math.floor(Math.random() * this.tetrominos.length)]));
   }
 
   checkLine = () =>  {
-    // past.forEach(function(tetromino) {console.log(tetromino.blocks)});
-
-    for (let yline = 600.5; yline > 0; yline -= this.p) {
+    for (let yline = this.floor; yline > 0; yline -= this.p) {
         let count = 0;
-        for (let xline = 0.5; xline <= 380.5; xline += this.p) {
+        for (let xline = 0.5; xline <= this.rightBorder; xline += this.p) {
 
 
             for (let i = 0; i < this.past.length; i++) {
                 for (let j = 0; j < this.past[i].blocks.length; j++) {
                     if (this.past[i].blocks[j][1] == yline && this.past[i].blocks[j][0] == xline) {
-                        // xline +=p;
                         count++;
-                        if (count == 20) this.destroyLine(yline);
+                        if (count == this.width) this.destroyLine(yline);
                         break;
 
                     }
@@ -210,30 +231,22 @@ export class TetrisComponent implements OnInit, AfterViewInit {
             if (this.past[i].blocks[j][1] == yline) {
                 xarray.push(this.past[i].blocks[j][0]);
                 this.past[i].blocks.splice(j, 1);
-                // for (let k = 0; k < past.length; k++) {
-                //     for (let m = 0; m < past[k].blocks.length; m++) {
-                //         if(past[k].blocks[m][0] == xcoord ) past[k].blocks[m][1]+=p;
-                //     }
-                // }
-                
                 j--;
             }
         }
     }
-    for (let k = 0; k < this.past.length; k++) {
-        for (let m = 0; m < this.past[k].blocks.length; m++) {
-            if( xarray.includes(this.past[k].blocks[m][0]) ) this.past[k].blocks[m][1]+=this.p;
+        for (let i = 0; i < this.past.length; i++) {
+            for (let j = 0; j < this.past[i].blocks.length; j++) {
+                if (this.past[i].blocks[j][1] < yline){
+                    this.past[i].blocks[j][1] += this.p;
+                }
+            }
         }
     }
-  }
 
-  endGame() {
-    clearInterval(this.gameDown);
-    clearInterval(this.gameRefresh);
-    window.alert(`Game Over Score: ${this.score}`);
-  }
+  
 
-  checkCollision2 =  () => {
+  checkCollisionWithoutDelay =  () => {
     for (let i = 0; i < this.past.length; i++) {
         for (let j = 0; j < this.past[i].blocks.length; j++) {
             if (this.past[i].blocks[j][0] == this.current.blocks[0][0] && this.past[i].blocks[j][1] == this.current.blocks[0][1] + this.p
@@ -246,17 +259,10 @@ export class TetrisComponent implements OnInit, AfterViewInit {
                             this.endGame();
                             return true;
                         }
-                        
                     }
                     this.storetetromino(this.current);
-                    // this.inDelay = false;
-                    
-                    
-                    this.begin();
+                    this.continue();
                     return true;
-                    // return true;
-                    
-                    
             }
         }
     }
@@ -265,20 +271,12 @@ export class TetrisComponent implements OnInit, AfterViewInit {
             if(this.inDelay == false){
 
                 this.storetetromino(this.current);
-                
-                
-                this.begin();
+                this.continue();
                 return true;
-                // return true;
             }
-            //   clearInterval(this.gameDown);
-            //   clearInterval(this.gameRefresh);
-            //   await this.delay(500);
-            //   this.storetetromino(this.current);
-            //   this.begin();
         }
     }
-    this.begin();
+    this.continue();
     return false;
   }
 
@@ -290,12 +288,9 @@ export class TetrisComponent implements OnInit, AfterViewInit {
                 || this.past[i].blocks[j][0] == this.current.blocks[2][0] && this.past[i].blocks[j][1] == this.current.blocks[2][1] + this.p
                 || this.past[i].blocks[j][0] == this.current.blocks[3][0] && this.past[i].blocks[j][1] == this.current.blocks[3][1] + this.p) {
 
-                    for (let i = 0; i < 4; i++) {
-                        if (0.5 > this.current.blocks[i][1]) {
-                            this.endGame();
-                            return true;
-                        }
-                        
+                    if (0.5 > this.current.blocks[0][1]) {
+                        this.endGame();
+                        return true;
                     }
                     if(this.inDelay == false){
 
@@ -303,12 +298,8 @@ export class TetrisComponent implements OnInit, AfterViewInit {
                         clearInterval(this.gameRefresh);
                         this.inDelay = true;
                         await this.delay(this.tickrate);
-                        // this.storetetromino(this.current);
                         this.inDelay = false;
-                        this.checkCollision2();
-                        
-                        
-                        // this.begin();
+                        this.checkCollisionWithoutDelay();
                         return true;
                     }
             }
@@ -322,23 +313,12 @@ export class TetrisComponent implements OnInit, AfterViewInit {
                 clearInterval(this.gameRefresh);
                 this.inDelay = true;
                 await this.delay(this.tickrate);
-                // setTimeout(function() {this.storetetromino(this.current)}, this.tickrate);
-                // this.storetetromino(this.current);
                 this.inDelay = false;
-                this.checkCollision2();
-                
-                
-                // this.begin();
+                this.checkCollisionWithoutDelay();
                 return true;
             }
-            //   clearInterval(this.gameDown);
-            //   clearInterval(this.gameRefresh);
-            //   await this.delay(500);
-            //   this.storetetromino(this.current);
-            //   this.begin();
         }
     }
-    
     return false;
   }
 
@@ -514,7 +494,6 @@ export class TetrisComponent implements OnInit, AfterViewInit {
         }
 
     }
-    //600.5, 380.5, 0.5 = coordinates of board
     for (let i = 0; i < 4; i++) {
         if (this.rightBorder < proposed.blocks[i][0]
             || this.leftBorder > proposed.blocks[i][0]) {
