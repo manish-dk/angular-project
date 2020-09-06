ed6ceca7d31f5846088c2a97ad8c6a6c28f89fee
Manish Kotecha <manish@Manishs-MacBook-Pro.local>
2020-09-06 08:19:38 +0100

    edit about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,9 +11,9 @@ export class AboutComponent implements OnInit {
   siteSummary: string =
     "I currently work as a QA Engineer and my day to day involves doing a lot of C# and the work I do in fixing and extending the automation framework is much more akin to back-end development so I created this site as a way to teach myself a front-end framework. This site was made using Angular; I chose this framework as it's the framework that's currently used at my company for most of our web apps.";
   algoSummary: string =
-    "Due to having studied Maths & Physics instead of Computer Science I hadn't had much exposure to algorithms. I created this algorithm visualiser as a way to learn a bit more about algorithms. I've currently got a visualisation for 3 different ones but there may be more to come in the future.";
+    "Due to having studied Maths & Physics instead of Computer Science I hadn't had much exposure to algorithms. I created this algorithm visualiser as a way to learn a bit more about them. I currently have a visualisation for 3 different ones but there may be more to come in the future.";
   tetrisSummary: string =
-    "I started created this Tetris game a little while ago as a way to teach myself some JavaScript. This was initially incomplete and very buggy. When I was building this site I also decided to go back to it and it is now a fully functional tetris game with a couple of additional mechanics.";
+    "I started this Tetris game a little while ago as a way to teach myself some JavaScript when I was still relatively new to coding. This was initially incomplete and very buggy. When I was building this site I decided to go back to it and it is now a fully functional tetris game with a couple of additional mechanics.";
   constructor() {}
 
   ngOnInit(): void {
